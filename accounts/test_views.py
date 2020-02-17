7a454c90a01437869ec2a3f502ae71d893d82778
Tomasz Cygal <tomaszcygal@yahoo.com>
2020-02-17 12:06:19 +0000

    first test done for index.html

---
+++ accounts/test_views.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+
+class TestViews(TestCase):
+    def test_get_home_page(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
--- nutristore/urls.py
+++ nutristore/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from accounts.views import index
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    
+    url(r'^$', index),
 ]
