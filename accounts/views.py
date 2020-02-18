56bf95d8b08abaa889cea317c40865fc9ddeb83a
Tomasz Cygal <tomaszcygal@yahoo.com>
2020-02-18 11:58:13 +0000

    registration page started, no form done yet

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 # Create your views here.
 
@@ -7,6 +8,7 @@ def index(request):
     """Return the index.html file"""
     return render(request, 'index.html')
 
+@login_required
 def logout(request):
     """Log the user out"""
     auth.logout(request)
@@ -15,6 +17,8 @@ def logout(request):
 
 def login(request):
     """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
@@ -26,8 +30,13 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
+
+def registration(request):
+    """Render the registration page"""
+    return render(request, 'registration.html')
--- nutristore/urls.py
+++ nutristore/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from accounts.views import index, logout, login
+from accounts.views import index, logout, login, registration
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name= "index"),
     url(r'^accounts/logout/$', logout, name="logout"),
     url(r'^accounts/login/$', login, name= "login"),
+    url(r'^accounts/register/$', registration, name = "registration")
 ]
