e672904c457806dfc23bc49d793f662da54b9d85
Tomasz Cygal <tomaszcygal@yahoo.com>
2020-02-23 12:03:27 +0000

    added return to product category

--- products/models.py
+++ products/models.py
@@ -16,6 +16,7 @@ class ProductCategory(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
 
-    
+    def __str__(self):
+        return self.name
 
 
