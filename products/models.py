f5d1e7909a3b139d97e8cd2442f696d15e0d9bc8
Tomasz Cygal <tomaszcygal@yahoo.com>
2020-02-23 12:01:34 +0000

    upgrades done to pillow and django

--- products/models.py
+++ products/models.py
@@ -9,9 +9,13 @@ class Product(models.Model):
     category_id = models.IntegerField()
     quantity = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 class ProductCategory(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.name
+    
+
+
