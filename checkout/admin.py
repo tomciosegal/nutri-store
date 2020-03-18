ecc0ab882943f3422005e8d0970b65bffd328916
Tomasz Cygal <tomaszcygal@yahoo.com>
2020-03-18 10:56:16 +0000

    order history OrderItemHistory, OrderHistory added to imports  in admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderLineItem
+from .models import Order, OrderLineItem, OrderItemHistory, OrderHistory
 
 
 class OrderLineAdminInline(admin.TabularInline):
