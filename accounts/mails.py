2d03bfb37cd58ef6a26754fe51734749a2313186
Tomasz Cygal <tomaszcygal@yahoo.com>
2020-04-06 16:41:58 +0000

    email failed contact form voided with try and exept

--- accounts/mails.py
+++ accounts/mails.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from smtplib import SMTPAuthenticationError
+from django.contrib import messages
 
 
 def send_contact_mail(request):
@@ -7,11 +9,15 @@ def send_contact_mail(request):
     this function send subscribtion to you email with order
     """
     data = request.POST
-    send_mail(
-        f'New message from {data["email"]}',
-        f"You have new message: {data['message']}\nFrom: {data['name']}",
-        settings.FROM_EMAIL,
-        ['tomaszcygal@yahoo.com'],
-        fail_silently=False,
-        
-    )
+    try:
+        send_mail(
+            f'New message from {data["email"]}',
+            f"You have new message: {data['message']}\nFrom: {data['name']}",
+            settings.FROM_EMAIL,
+            ['tomaszcygal@yahoo.com'],
+            fail_silently=False,
+        )
+    except SMTPAuthenticationError:
+        messages.error(request, "Your order confirmation email send failed")
+    
+    
